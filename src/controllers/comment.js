a42e995b7cd84b7ef0f3f0a6c7c02285548d1a30
Hila <hila.ohana1910@gmail.com>
2024-12-12 18:19:48 +0200

    upadte  commenr query

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -28,21 +28,45 @@ const getCommentById = async (req, res) => {
     } catch (error) {
       res.status(500).json({ message: 'Error fetching comment', error });
     }
-  };
+};
 
-  // CREATE a new comment
+// CREATE a new comment
 const createComment = async (req, res) => {
-    const { content, author } = req.body;
+  const { content, author } = req.body;
   
-    try {
-      const newComment = new Comment({
-        content,
-        author,
-      });
+  try {
+    const newComment = new Comment({
+      content,
+      author,
+    });
   
-      await newComment.save();
-      res.status(201).json(newComment);
-    } catch (error) {
-      res.status(500).json({ message: 'Error creating comment', error });
+    await newComment.save();
+    res.status(201).json(newComment);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating comment', error });
+  }
+};
+
+// UPDATE a comment by ID
+const updateComment = async (req, res) => {
+  const { id } = req.params;
+  const { content, author } = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "Invalid id" });
+  }
+  
+  try {
+    const updatedComment = await Comment.findByIdAndUpdate(
+      id,
+      { content, author },
+      { new: true }
+    );
+    if (!updatedComment) {
+      return res.status(404).json({ message: 'Comment not found' });
     }
-  };
+    res.status(200).json(updatedComment);
+  } catch (error) {
+    res.status(500).json({ message: 'Error updating comment', error });
+  }
+};
