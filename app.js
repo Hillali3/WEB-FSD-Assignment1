b8d4a173edb4e625cd5022222ffd6e448cbab4d6
Hila <hila.ohana1910@gmail.com>
2024-12-12 18:23:47 +0200

    add request.rest file

--- app.js
+++ app.js
@@ -14,11 +14,9 @@ db.once("open", () => console.log("connected to database"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const postsRoute = require("./src/routes/posts_route");
 const commentsRoute = require("./src/routes/comment_route");
 
 app.use('/comments', commentsRoute);
-app.use("/posts", postsRoute);
 
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
