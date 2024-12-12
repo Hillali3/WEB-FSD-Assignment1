8cbea43c26533bd642493ab20eb6503dcb33fe23
Hila <hila.ohana1910@gmail.com>
2024-12-12 18:16:32 +0200

    getCommentById query

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -10,3 +10,24 @@ const getComments = async (req, res) => {
     res.status(500).json({ message: 'Error fetching comments', error });
   }
 };
+
+// GET comment by ID
+const getCommentById = async (req, res) => {
+    const { id } = req.params;
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid id" });
+    }
+    
+    try {
+      const comment = await Comment.findById(id);
+      if (!comment) {
+        return res.status(404).json({ message: 'Comment not found' });
+      }
+      res.status(200).json(comment);
+    } catch (error) {
+      res.status(500).json({ message: 'Error fetching comment', error });
+    }
+  };
+
+  
