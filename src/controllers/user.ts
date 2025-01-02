7bc2cd3bb8cbac73fb58ed06dee62dd43a676246
Hila <hila.ohana1910@gmail.com>
2025-01-02 16:13:47 +0200

    get all users

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -30,3 +30,90 @@ export const createUser = async (req: Request, res: Response) => {
     return sendError(res, "Fail registration");
   }
 };
+
+// Get  all users
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: "Error fetching users", error });
+  }
+};
+
+//   // Get user by id
+//   export const getUserById = async (req: Request, res: Response) => {
+//     const { id } = req.params;
+
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       return res.status(400).json({ error: "Invalid id" });
+//     }
+
+//     try {
+//       const user = await User.findById(id);
+//       if (!user) {
+//         return res.status(404).json({ message: "User is not found" });
+//       }
+//       res.status(200).json(user);
+//     } catch (error) {
+//       res.status(500).json({ message: "Error fetching user", error });
+//     }
+//   };
+
+//   // Get user by username
+//   export const getUserByUsername = async (req: Request, res: Response) => {
+//     const { username } = req.params;
+
+//     try {
+//       const user = await User.find({ username });
+//       if (user.length === 0) {
+//         return res
+//           .status(404)
+//           .json({ message: "No user found for this username" });
+//       }
+//       return res.status(200).json(user);
+//     } catch (error) {
+//       return res.status(500).json({ message: "Error fetching users", error });
+//     }
+//   };
+
+//   //update user by id
+//   export const updateUser = async (req: Request, res: Response) => {
+//     const id = req.params.id;
+//     const { name, password, email, birthDate, username } = req.body;
+
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       return res.status(400).json({ error: "Invalid id" });
+//     }
+
+//     try {
+//       const updatedUser = await User.findByIdAndUpdate(
+//         id,
+//         { name, password, email, birthDate, username },
+//         { new: true }
+//       );
+//       if (!updatedUser) {
+//         return res.status(404).json({ message: "User not found" });
+//       }
+//       res.status(200).json(updatedUser);
+//     } catch (error) {
+//       res.status(500).json({ message: "Error updating user", error });
+//     }
+//   };
+
+//   // Delete user by id
+//   export const deleteUser = async (req: Request, res: Response) => {
+//     const id = req.params.id;
+
+//     try {
+//       const deletedUser = await User.findByIdAndDelete(id);
+
+//       if (!deletedUser) {
+//         return res.status(404).json({ message: "User not found" });
+//       }
+
+//       return res.status(200).json({ message: "user deleted successfully" });
+//     } catch (error) {
+//       return res.status(500).json({ message: "Error deleting user", error });
+//     }
+//   };
