32065b22ca9a4f90cf95eb17afa4ae7caf08d2d6
Hila <hila.ohana1910@gmail.com>
2025-01-02 16:02:02 +0200

    generateAccessToken

---
+++ src/utils/sendError.ts
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+// Load environment variables
+import dotenv from "dotenv";
+import User from "../models/user";
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET as string;
+
+// Generate Access Token
+export const generateAccessToken = (userId: any): string => {
+  return jwt.sign({ userId }, JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
